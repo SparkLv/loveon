12875379b8d5a3a8ae1f13c9116c5808ed26cf5b
lvwenbin@youyuwo.com <lvwenbin@youyuwo.com>
2018-08-14 11:48:18 +0800

    导航

--- pages/add/index.js
+++ pages/add/index.js
@@ -1,6 +1,17 @@
 import React, { Component } from "react";
 import { StyleSheet, View, Text, Button, Image } from "react-native";
+
 export default class Add extends Component {
+  componentDidMount() {
+    // console.log(ImagePicker);
+    // ImagePicker.openPicker({
+    //   width: 300,
+    //   height: 400,
+    //   cropping: true
+    // }).then(image => {
+    //   console.log(image);
+    // });
+  }
   render() {
     return (
       <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
--- pages/cover.js
+++ pages/cover.js
@@ -2,28 +2,15 @@ import React, { Component } from "react";
 
 import { StyleSheet, View, Text, TouchableOpacity, ImageBackground } from "react-native";
 
-// import "whatwg-fetch";
-
-import axios from "axios";
-
 export default class Cover extends Component {
   constructor() {
     super();
     this.state = {
-      time: 5
+      time: 5,
     };
   }
   componentDidMount() {
     this.timer = setInterval(this.reduceTime.bind(this), 1000);
-    navigator.geolocation.getCurrentPosition(function(info) {
-      const url = `https://free-api.heweather.com/s6/weather/now?location=${info.coords.latitude},${info.coords.longitude}&key=1f588e1a434d45e981f079c3e7790ed1`;
-      // fetch(url,{method:'get'}).then(res=>{
-      //   alert(JSON.stringify(res._bodylnit));
-      // })
-      axios.get(url).then(res => {
-        alert(JSON.stringify(res));
-      });
-    });
   }
   reduceTime() {
     if (this.state.time === 1) {
